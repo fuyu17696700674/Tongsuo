577f3a52198303e23e69b726650fc1dbf47fbbf9
fu-dc <1060062785@qq.com>
2025-03-02 10:42:19 +0800

    add nonlinearwbsm4

--- crypto/nonlinearwbsm4/nonlinearwbsm4.c
+++ crypto/nonlinearwbsm4/nonlinearwbsm4.c
@@ -2,95 +2,6 @@
 #include <string.h> 
 #include "crypto/sm4.h"
 
-// //====sm4轮密钥生成====
-// #   define ossl_inline inline
-// # define SM4_KEY_SCHEDULE  32
-// typedef struct SM4_KEY_st {
-//     uint32_t rk[SM4_KEY_SCHEDULE];
-// } SM4_KEY;
-// static const uint8_t SM4_S[256] = {
-//         0xD6, 0x90, 0xE9, 0xFE, 0xCC, 0xE1, 0x3D, 0xB7, 0x16, 0xB6, 0x14, 0xC2,
-//         0x28, 0xFB, 0x2C, 0x05, 0x2B, 0x67, 0x9A, 0x76, 0x2A, 0xBE, 0x04, 0xC3,
-//         0xAA, 0x44, 0x13, 0x26, 0x49, 0x86, 0x06, 0x99, 0x9C, 0x42, 0x50, 0xF4,
-//         0x91, 0xEF, 0x98, 0x7A, 0x33, 0x54, 0x0B, 0x43, 0xED, 0xCF, 0xAC, 0x62,
-//         0xE4, 0xB3, 0x1C, 0xA9, 0xC9, 0x08, 0xE8, 0x95, 0x80, 0xDF, 0x94, 0xFA,
-//         0x75, 0x8F, 0x3F, 0xA6, 0x47, 0x07, 0xA7, 0xFC, 0xF3, 0x73, 0x17, 0xBA,
-//         0x83, 0x59, 0x3C, 0x19, 0xE6, 0x85, 0x4F, 0xA8, 0x68, 0x6B, 0x81, 0xB2,
-//         0x71, 0x64, 0xDA, 0x8B, 0xF8, 0xEB, 0x0F, 0x4B, 0x70, 0x56, 0x9D, 0x35,
-//         0x1E, 0x24, 0x0E, 0x5E, 0x63, 0x58, 0xD1, 0xA2, 0x25, 0x22, 0x7C, 0x3B,
-//         0x01, 0x21, 0x78, 0x87, 0xD4, 0x00, 0x46, 0x57, 0x9F, 0xD3, 0x27, 0x52,
-//         0x4C, 0x36, 0x02, 0xE7, 0xA0, 0xC4, 0xC8, 0x9E, 0xEA, 0xBF, 0x8A, 0xD2,
-//         0x40, 0xC7, 0x38, 0xB5, 0xA3, 0xF7, 0xF2, 0xCE, 0xF9, 0x61, 0x15, 0xA1,
-//         0xE0, 0xAE, 0x5D, 0xA4, 0x9B, 0x34, 0x1A, 0x55, 0xAD, 0x93, 0x32, 0x30,
-//         0xF5, 0x8C, 0xB1, 0xE3, 0x1D, 0xF6, 0xE2, 0x2E, 0x82, 0x66, 0xCA, 0x60,
-//         0xC0, 0x29, 0x23, 0xAB, 0x0D, 0x53, 0x4E, 0x6F, 0xD5, 0xDB, 0x37, 0x45,
-//         0xDE, 0xFD, 0x8E, 0x2F, 0x03, 0xFF, 0x6A, 0x72, 0x6D, 0x6C, 0x5B, 0x51,
-//         0x8D, 0x1B, 0xAF, 0x92, 0xBB, 0xDD, 0xBC, 0x7F, 0x11, 0xD9, 0x5C, 0x41,
-//         0x1F, 0x10, 0x5A, 0xD8, 0x0A, 0xC1, 0x31, 0x88, 0xA5, 0xCD, 0x7B, 0xBD,
-//         0x2D, 0x74, 0xD0, 0x12, 0xB8, 0xE5, 0xB4, 0xB0, 0x89, 0x69, 0x97, 0x4A,
-//         0x0C, 0x96, 0x77, 0x7E, 0x65, 0xB9, 0xF1, 0x09, 0xC5, 0x6E, 0xC6, 0x84,
-//         0x18, 0xF0, 0x7D, 0xEC, 0x3A, 0xDC, 0x4D, 0x20, 0x79, 0xEE, 0x5F, 0x3E,
-//         0xD7, 0xCB, 0x39, 0x48
-// };
-// static ossl_inline uint32_t load_u32_be(const uint8_t *b, uint32_t n)
-// {
-//     return ((uint32_t)b[4 * n] << 24) |
-//            ((uint32_t)b[4 * n + 1] << 16) |
-//            ((uint32_t)b[4 * n + 2] << 8) |
-//            ((uint32_t)b[4 * n + 3]);
-// }
-// static ossl_inline uint32_t rotl(uint32_t a, uint8_t n)
-// {
-//     return (a << n) | (a >> (32 - n));
-// }
-// int ossl_sm4_set_key(const uint8_t *key, SM4_KEY *ks)
-// {
-//     /*
-//      * Family Key
-//      */
-//     static const uint32_t FK[4] =
-//             { 0xa3b1bac6, 0x56aa3350, 0x677d9197, 0xb27022dc };
-
-//     /*
-//      * Constant Key
-//      */
-//     static const uint32_t CK[32] = {
-//             0x00070E15, 0x1C232A31, 0x383F464D, 0x545B6269,
-//             0x70777E85, 0x8C939AA1, 0xA8AFB6BD, 0xC4CBD2D9,
-//             0xE0E7EEF5, 0xFC030A11, 0x181F262D, 0x343B4249,
-//             0x50575E65, 0x6C737A81, 0x888F969D, 0xA4ABB2B9,
-//             0xC0C7CED5, 0xDCE3EAF1, 0xF8FF060D, 0x141B2229,
-//             0x30373E45, 0x4C535A61, 0x686F767D, 0x848B9299,
-//             0xA0A7AEB5, 0xBCC3CAD1, 0xD8DFE6ED, 0xF4FB0209,
-//             0x10171E25, 0x2C333A41, 0x484F565D, 0x646B7279
-//     };
-
-//     uint32_t K[4];
-//     int i;
-
-//     K[0] = load_u32_be(key, 0) ^ FK[0];
-//     K[1] = load_u32_be(key, 1) ^ FK[1];
-//     K[2] = load_u32_be(key, 2) ^ FK[2];
-//     K[3] = load_u32_be(key, 3) ^ FK[3];
-
-//     for (i = 0; i != SM4_KEY_SCHEDULE; ++i) {
-//         uint32_t X = K[(i + 1) % 4] ^ K[(i + 2) % 4] ^ K[(i + 3) % 4] ^ CK[i];
-//         uint32_t t = 0;
-
-//         t |= ((uint32_t)SM4_S[(uint8_t)(X >> 24)]) << 24;
-//         t |= ((uint32_t)SM4_S[(uint8_t)(X >> 16)]) << 16;
-//         t |= ((uint32_t)SM4_S[(uint8_t)(X >> 8)]) << 8;
-//         t |= SM4_S[(uint8_t)X];
-
-//         t = t ^ rotl(t, 13) ^ rotl(t, 23);
-//         K[i % 4] ^= t;
-//         ks->rk[i] = K[i % 4];
-//     }
-
-//     return 1;
-// }
-
-//
 uint8_t  SBOX[256] = {
     0xd6, 0x90, 0xe9, 0xfe, 0xcc, 0xe1, 0x3d, 0xb7,
     0x16, 0xb6, 0x14, 0xc2, 0x28, 0xfb, 0x2c, 0x05,
@@ -249,44 +160,6 @@ uint32_t nonlinearU32(const Nonlinear32* n32, uint32_t arr) {
     // 重新拼接 4 个字节，形成一个新的 32 位数
     return (new_byte1 << 24) | (new_byte2 << 16) | (new_byte3 << 8) | new_byte4;
 }
-//测试函数
-//void printNonlinear32(Nonlinear32* nl32) {
-//    for (int i = 0; i < 256; i++) {
-//        // 打印拼接后的 32-bit 映射值
-//        printf("Mapping[%d]: 0x%02X%02X%02X%02X\n", i,
-//            nl32->n8_1.mapping[i],
-//            nl32->n8_2.mapping[i],
-//            nl32->n8_3.mapping[i],
-//            nl32->n8_4.mapping[i]);
-//    }
-//}
-
-
-//void printNonlinear8(Nonlinear8* nl8) {
-//    for (int i = 0; i < 256; i++) {
-//        printf("Mapping[%d]: 0x%02X\n", i, nl8->mapping[i]);
-//    }
-//}
-//void printNonlinear32WithDetails(Nonlinear8 n1, Nonlinear8 n2, Nonlinear8 n3, Nonlinear8 n4, Nonlinear32* n32) {
-//    for (int i = 0; i < 256; i++) {
-//        // 打印拼接前的四个 8 位映射
-//        printf("Mapping[%d]: n1 = 0x%02X, n2 = 0x%02X, n3 = 0x%02X, n4 = 0x%02X -> ",
-//            i, n1.mapping[i], n2.mapping[i], n3.mapping[i], n4.mapping[i]);
-//        // 拼接后的 32 位值
-//        uint32_t result = (n1.mapping[i] << 24) | (n2.mapping[i] << 16) | (n3.mapping[i] << 8) | n4.mapping[i];
-//        printf("n1 << 24 | n2 << 16 | n3 << 8 | n4 = 0x%08X\n", result);
-//        // 打印 Nonlinear32 中的拼接后的值
-//        printf("Result in Nonlinear32: 0x%08X\n", n32->n8_1.mapping[i] << 24 | n32->n8_2.mapping[i] << 16 | n32->n8_3.mapping[i] << 8 | n32->n8_4.mapping[i]);
-//    }
-//}
-// 打印非线性双射对
-//void printNonlinearPair8(Nonlinear8* nl, Nonlinear8* nl_inv) {
-//    for (int i = 0; i < 256; i++) {
-//        // 显示映射和逆映射
-//        printf("Mapping: %u -> %u, Inverse Mapping: %u -> %u\n",
-//            i, nl->mapping[i], nl_inv->mapping[nl->mapping[i]], nl->mapping[i]);
-//    }
-//}
 
 M32 L_matrix = {
     .M[0] = 0xA0202080,
@@ -464,9 +337,6 @@ static void wbsm4_gen_part3(WB_SM4_Tables* tables) {
             }
         }
     }
-    //save_array_8bit("./tables/part3_table1.bin", (uint8_t*)part3_table1, 32 * 4 * 256);
-    //save_array_8bit("./tables/part3_table1_dec.bin", (uint8_t*)part3_table1_dec, 32 * 4 * 256);
-    //save_array_8bit("./tables/part3_table2.bin", (uint8_t*)part3_table2, 32 * 4 * 256);
 }
 //共32轮，每轮有4张输入16bit，输出8bit的查找表
 static void wbsm4_gen_part4_1(WB_SM4_Tables* tables) {
